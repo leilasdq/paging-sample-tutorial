67d35e351cd12694d1acc4e294e9c02ef8c5991e
leila_sdq <leila.sedighi.2020@gmail.com>
2022-07-17 22:12:43 +0430

    create network module

---
+++ app/src/main/java/com/example/pagingsample/di/NetworkModule.kt
@@ -0,0 +1,31 @@
+package com.example.pagingsample.di
+
+import com.example.pagingsample.network.MoviesService
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val baseUrl = "http://moviesapi.ir/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideMoviesService(retrofit: Retrofit): MoviesService =
+        retrofit.create(MoviesService::class.java)
+}
