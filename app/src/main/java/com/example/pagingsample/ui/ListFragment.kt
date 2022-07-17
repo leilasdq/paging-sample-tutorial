4593667412dba65ee7748c6adaae9de030dec69c
leila_sdq <leila.sedighi.2020@gmail.com>
2022-07-17 23:49:58 +0430

    set listFragment as starter fragment

--- app/src/main/java/com/example/pagingsample/ui/ListFragment.kt
+++ app/src/main/java/com/example/pagingsample/ui/ListFragment.kt
@@ -2,13 +2,16 @@ package com.example.pagingsample.ui
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.fragment.app.viewModels
+import com.example.pagingsample.R
 
 @AndroidEntryPoint
-class ListFragment: Fragment() {
+class ListFragment: Fragment(R.layout.fragment_kist) {
 
     private val TAG = "leila_Movies"
     private val viewModel: ListViewModel by viewModels()
--- app/src/main/java/com/example/pagingsample/ui/MainActivity.kt
+++ app/src/main/java/com/example/pagingsample/ui/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.pagingsample.R
 import com.example.pagingsample.data.MoviesRepository
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import javax.inject.Inject
