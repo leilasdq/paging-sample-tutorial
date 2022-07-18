546f851bc8e27fc47a847af7e376131d7a15b0f7
leila_sdq <leila.sedighi.2020@gmail.com>
2022-07-18 11:11:39 +0430

    refactor names

--- app/src/main/java/com/example/pagingsample/ui/MovieListFragment.kt
+++ app/src/main/java/com/example/pagingsample/ui/MovieListFragment.kt
@@ -2,16 +2,14 @@ package com.example.pagingsample.ui
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.fragment.app.viewModels
 import com.example.pagingsample.R
 
 @AndroidEntryPoint
-class ListFragment: Fragment(R.layout.fragment_kist) {
+class MovieListFragment: Fragment(R.layout.fragment_movie_list) {
 
     private val TAG = "leila_Movies"
     private val viewModel: ListViewModel by viewModels()
